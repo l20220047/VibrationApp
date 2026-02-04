64081286a9edf7fed7adc5770c4dc40d232eeaf9
Pham Tuan Hung <L20220047@prumyslovkajihlava.cz>
2026-02-04 09:07:03 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 
 function App() {
   
+  const [delkaVibrace, setDelkaVibrace] = useState(100);
+
   function zavibruj(){
     if("vibrate" in navigator){
       //zarizeni podporuje vibrace
@@ -26,13 +28,36 @@ function App() {
       alert("Toto zařízení nepodporuje vibrace.")
     }
   }
+
+  function zavibrujCas(time){
+    if("vibrate" in navigator){
+      //zarizeni podporuje vibrace
+      //funkce pro vibrace ,delka vibrace je v ms
+      navigator.vibrate(time);
+    }
+    else{
+      //zarizeni nepodporuje vibrace
+      alert("Toto zařízení nepodporuje vibrace.")
+    }
+  }
     
 
   return (
     <div>
       <h1>Vibration App</h1>
       <button onClick={zavibruj}>Vibrate</button>
+      <button onClick={zavibrujSOS}>Vibrate SOS</button>
+      <button onClick={zavibrujCas(1000)}>Vibrate 1s</button>
+
+      <label>
+        délka vibrace: [delkaVibrace]ms
+      </label>
+      <input type="range" min="100" max="5000" value={delkaVibrace} />
+      onchange={ (e) => setDelkaVibrace(Number(e.target.value)) } 
     </div>
+    
+
+    
   )
 }
 
